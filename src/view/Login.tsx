5c6124a1591ec7ced668b641030c715ae0fa89b4
Erick <erickfthomaz@gmail.com>
2023-04-08 13:46:54 -0300

    Adicionando responsividade

--- src/view/Login.tsx
+++ src/view/Login.tsx
@@ -19,44 +19,44 @@ export default function Login() {
     return (
         <>
             <div className="flex flex-row text-base font-poppins">
-                <div className="flex flex-col bg-base w-1/2">
+                <div className="flex h-screen w-screen flex-col bg-base md:flex md:w-1/2 md:flex-col">
                     <div className="m-6">
                         <img src={logo}/>
                     </div>
 
-                    <div className="flex flex-col m-auto bg-form xl:w-[600px] xl:h-[500px] rounded">
+                    <div className="mt-5 flex flex-col rounded h-[600px] md:bg-form md:w-[600px] md:h-[550px] md:m-auto gap-3">
 
-                        <div className="flex flex-col mt-[30px] mx-auto gap-1 p-2">
+                        <div className="mx-auto flex flex-col gap-3 p-2 mt-[30px] self-start">
                             <div className="flex flex-row gap-2">
                                 <img className="w-[25px]" src={login}/>
                                 <h1 className="text-2xl">Faça seu login</h1>
                             </div>
-                            <p className="text-sm">Entre com suas informações de cadastro.</p>
+                            <p className="text-sm mx-3">Entre com suas informações de cadastro.</p>
                         </div>
 
 
-                        <div className="flex flex-col mx-auto p-3">
-                            <form className="flex flex-col">
+                        <div className="mx-auto flex flex-col p-3">
+                            <form className="flex flex-col gap-3">
                                 <div className="flex flex-col">
-                                    <label className="font-bold py-2">E-mail</label>
+                                    <label className="py-2 font-bold">E-mail</label>
                                     <div
                                         className={`flex items-center`}>
                                         <img className="absolute mx-1" src={emailSelect ? emailActive : email}/>
                                         <input
-                                            className="pl-6 bg-form outline-none w-full focus:border-prim border-2 rounded py-2 px-2 border-white hover:border-prim"
+                                            className="w-full rounded border-2 border-white px-2 py-2 pl-6 outline-none bg-form hover:border-prim focus:border-prim"
                                             type="email"
                                             placeholder="Digite seu email"
                                             onSelect={() => setEmailSelected(true)}/>
                                     </div>
                                 </div>
 
-                                <div className="flex flex-col mt-3">
-                                    <label className="font-bold py-2">Senha</label>
+                                <div className="mt-3 flex flex-col">
+                                    <label className="py-2 font-bold">Senha</label>
                                     <div
                                         className={"flex relative items-center"}>
                                         <img className="absolute mx-1" src={passwordSelect ? lockActive : lock} alt=""/>
                                         <input
-                                            className="pl-6 bg-form outline-none w-full focus:border-prim border-2 rounded py-2 px-2 border-white hover:border-prim"
+                                            className="w-full rounded border-2 border-white px-2 py-2 pl-6 outline-none bg-form hover:border-prim focus:border-prim"
                                             type={passwordShow ? "text" : "password"} placeholder="Digite sua senha"
                                             onSelect={() => setPasswordSelect(true)}/>
                                         <img className="absolute right-2 cursor-pointer"
@@ -65,31 +65,31 @@ export default function Login() {
                                     </div>
                                 </div>
 
-                                <div className="flex flex-row gap-5 p-2">
+                                <div className="flex flex-row gap-x-16 p-2">
                                     <div className="flex flex-row gap-2">
                                         <input className="bg-form text-prim accent-prim" type="checkbox"/>
                                         <p>Lembre-me</p>
                                     </div>
                                     <div>
-                                        <span><a className="font-bold text-prim text-sm"
+                                        <span><a className="text-sm font-bold text-prim"
                                                  href={"#"}>Esqueci minha senha</a></span>
                                     </div>
                                 </div>
 
                                 <div
-                                    className="flex center m-auto rounded bg-prim w-full hover:scale-95 hover:bg-prim2 transition cursor-pointer">
-                                    <input className="text-black m-auto font-poppins py-4 px-4 cursor-pointer"
+                                    className="m-auto flex w-full cursor-pointer rounded transition center bg-prim hover:bg-prim2 hover:scale-95">
+                                    <input className="m-auto cursor-pointer px-4 py-4 text-black font-poppins"
                                            type="submit"
                                            value="Entrar"/>
                                 </div>
                             </form>
                         </div>
-                        <a href={"#"} className="text-prim m-auto">Não tem uma conta? <span
-                            className="text-prim font-bold">Registre-se</span></a>
+                        <a href={"#"} className="m-auto text-prim">Não tem uma conta? <span
+                            className="font-bold text-prim">Registre-se</span></a>
                     </div>
                 </div>
-                <div className="w-1/2">
-                    <img className="xl:h-screen xl:w-screen" src="../../src/assets/side-image.jpg"/>
+                <div className="hidden md:relative md:block md:w-1/2">
+                    <img className="md:h-screen md:w-screen" src="../../src/assets/side-image.jpg"/>
                 </div>
             </div>
         </>
